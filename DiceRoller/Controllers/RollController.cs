af6515e6882df26a235091bb9ab4a21b1bc31418
Matthew Fotzler <mattf@pixotech.com>
2017-07-21 10:32:00 -0500

    initial commit

---
+++ DiceRoller/Controllers/RollController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiceRoller.Controllers
+{
+    [Route("api/[controller]")]
+    public class RollController : Controller
+    {
+        [HttpGet("")]
+        public JsonResult Get(int size, int count)
+        {
+            var random = new Random();
+            var rolls = Enumerable.Range(0, count)
+                .Select(x => random.Next(1, size))
+                .ToArray();
+
+            return Json(rolls);
+        }
+    }
+}
